ad0ea6c1ee841cf3be2153063522028bbbf1747e
jkbrime <jkbrime@yahoo.com>
2024-10-08 12:41:49 +0100

    my first python class

---
+++ class1.py
@@ -0,0 +1,55 @@
+number_of_boys = 7 #this is the number of boys in the class
+number_of_girls = 2 #this is the number of girls in the class
+my_name = 'jide'
+my_sex = 'male'
+age = 120
+fruit = 'mango'
+veggie = 'tomato'
+sport = 'football' #i love football, also known as soccer in the united states
+favourite_food = 'rice' #rice and fish stew
+favourite_drink = 'water' #i love soda water
+num1 = 14 #datatype integer
+num2 = 5
+sum = num1 + num2
+minus = num1 - num2
+times = num1 * num2
+divide = num1/num2
+modulus = num1 % num2 #to get the remainder
+round = num1 // num2 #to round up the answer
+print(round)  
+
+float_num = 20.9 #number with decimal point
+print(float_num)
+
+string_value = "my name is jkbrime"
+s = "and jide"
+print("my name is jkbrime", s)
+string_value
+#methods in string
+print(string_value)
+print(string_value.count("k")) #to count the number of k
+print(string_value.replace("j", "m"))#replace j with m
+print(string_value.upper()) #to convert string to upper class
+print(string_value.lower()) #to convert string to lower class
+print(string_value.islower())#to check if the string is lower class
+print(string_value.isupper()) #to check if the string is upper class
+print(string_value.__len__())#length of the string
+
+#inputs
+name = input("what is your name?")
+print("your name is" + name)
+
+#simple calculator to get addition and subtraction
+number1 = int(input("input any number")) # the int is to show that it is an integer
+number2 = int(input("input another number"))
+add = number1 + number2
+sub = number2 - number1
+print(f"the sum of your numbers are {add}") #f there is to show that it is an f string
+print(f"the substration of your numbers are {sub}")
+
+print(str(number))#output 78
+print(abs(-78))#output 78 this is the absolute value of the number
+print(max(1,2,3,4,5,)) #output 5 this is the maximum value of the numbers
+print(min(1,2,3,4,5)) #output 1 this is the minimum value of the numbers
+print(round(3.14)) #output 3 this is round up of the number
+print(round(3.78)) #output 4 this is round up of the number
